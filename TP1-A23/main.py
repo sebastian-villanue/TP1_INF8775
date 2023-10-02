f322a614bc3c048e61d6b4ccdc60b0276173b5ec
hdball04 <hannan.dissou@polymtl.ca>
2023-10-01 21:39:47 -0400

    finished all tests

--- TP1-A23/main.py
+++ TP1-A23/main.py
@@ -1,11 +1,11 @@
 import algorithms
 import time
 import os
+import sys
 
 
 
-
-def calculate_mean_time(tests, func):
+def calculate_mean_time_qs(tests, func):
     times = []
     for test in tests:
         start_time = time.time()
@@ -15,17 +15,37 @@ def calculate_mean_time(tests, func):
     return sum(times) / len(times) * 1000
 
 
+def calculate_mean_time_cs(tests, func):
+    times = []
+    for test in tests:
+        start_time = time.time()
+        func(test, len(test))
+        end_time = time.time()
+        times.append(end_time - start_time)
+    return sum(times) / len(times) * 1000
+
+
 if __name__ == "__main__":
+    sys.setrecursionlimit(5000)
+
     sample_files = os.listdir('samples')
     tests = []
 
     quicksort_1 = algorithms.quicksort_first_first
     quicksort_2 = algorithms.quicksort_experimental_first
     quicksort_3 = algorithms.quicksort_experimental_random
+    counting_sort = algorithms.counting_sort
+
 
     for sample_file in sample_files:
         with open(f'samples/{sample_file}', 'r') as file:
             tests.append([int(n) for n in file.read().split()])
 
-    mean_time = calculate_mean_time(tests, quicksort_1) 
-    print(mean_time)          
+    #mean_time_1 = calculate_mean_time_qs(tests, quicksort_1)
+    #mean_time_2 = calculate_mean_time_qs(tests, quicksort_2) 
+    #mean_time_3 = calculate_mean_time_qs(tests, quicksort_3)
+    mean_time_cs = calculate_mean_time_cs(tests, counting_sort)
+    print("Counting Sort: ", round(mean_time_cs , 3))
+    #print("Quicksort_1: ", round(mean_time_1 , 3))
+    #print("Quicksort_2: ", round(mean_time_2 , 3))
+    #print("Quicksort_3: ", round(mean_time_3 , 3))                             
